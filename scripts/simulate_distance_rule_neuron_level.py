46d0f4a46539c82d795d67206c3aae88c4978d82
janfb <jan.boelts@tum.de>
2022-04-07 19:27:28 +0200

    fix path handling in scripts.

--- scripts/simulate_distance_rule_neuron_level.py
+++ scripts/simulate_distance_rule_neuron_level.py
@@ -3,13 +3,17 @@ import torch
 
 
 from consbi.simulators import DistanceRuleSimulator
-
+from pathlib import Path
 from sbi.inference import simulate_for_sbi
 from sbi.utils import BoxUniform
 
 # set parameters
-path_to_model = "../data/structural_model"
-path_to_features = "../data/cube_model"
+BASE_DIR = Path(__file__).resolve().parent.parent.as_posix()
+path_to_model = BASE_DIR + "/data/structural_model"
+path_to_cellular_features = BASE_DIR + "/data/cube_model"
+path_to_subcellular_features = BASE_DIR + "/data/subcellular_features"
+save_folder = BASE_DIR + "/data"
+
 # set number of neuron pairs sampled from the connectome to mimick experimental settings, e.g., 50
 num_subsampling_pairs = 50
 cube_size = 50
@@ -23,7 +27,7 @@ save_data = True
 #     "set-5"  # Set 5 holds common cubes, min distance and axon-dendrite product for cube size 50mu-m
 
 prior = BoxUniform(torch.zeros(1), torch.ones(1) * 100)
-model = DistanceRuleSimulator(path_to_model, path_to_features,
+model = DistanceRuleSimulator(path_to_model, path_to_cellular_features,
                             feature_set_name="set-6", 
                             num_subsampling_pairs=num_subsampling_pairs, cube_size=cube_size)
 
@@ -37,5 +41,5 @@ def batch_simulator(theta):
 theta, x = simulate_for_sbi(batch_simulator, prior, num_simulations=num_simulations, num_workers=num_workers)
 
 if save_data:
-    with open(f"../data/presimulated_distance_rule_n{num_simulations}.p", "wb") as fh:
+    with open(save_folder + f"/presimulated_distance_rule_n{num_simulations}.p", "wb") as fh:
         pickle.dump(dict(prior=prior, theta=theta, x=x), fh)
--- scripts/simulate_dso.py
+++ scripts/simulate_dso.py
@@ -2,28 +2,29 @@ import pickle
 import torch
 
 from consbi.simulators import RuleSimulator, default_rule
+from pathlib import Path
 from torch.distributions import MultivariateNormal
 
 from sbi.inference import prepare_for_sbi, simulate_for_sbi
 
 # set parameters
-path_to_model = "../data/structural_model"
+BASE_DIR = Path(__file__).resolve().parent.parent.as_posix()
+path_to_model = BASE_DIR + "/data/structural_model"
+save_folder = BASE_DIR + "/data"
+save_data = True
+verbose = True
 # set number of neuron pairs sampled from the connectome to mimick experimental settings, e.g., 50
 num_subsampling_pairs = 50
-num_simulations = 10
-num_workers = 5
-save_data = True
-
+num_simulations = 10  # 1_000_000
+num_workers = 1 # 20
 
 prior = MultivariateNormal(torch.ones(3), torch.eye(3))
-simulator = RuleSimulator(path_to_model, default_rule, verbose=False, num_subsampling_pairs=num_subsampling_pairs)
+simulator = RuleSimulator(path_to_model, default_rule, verbose=verbose, num_subsampling_pairs=num_subsampling_pairs)
 
 batch_simulator, prior = prepare_for_sbi(simulator, prior)
 
 theta, x = simulate_for_sbi(batch_simulator, prior, num_simulations=num_simulations, num_workers=num_workers)
 
 if save_data:
-    with open(f"../data/presimulated_dso_rule_n{num_simulations}.p", "wb") as fh:
+    with open(save_folder + f"/presimulated_dso_rule_n{num_simulations}.p", "wb") as fh:
         pickle.dump(dict(prior=prior, theta=theta, x=x), fh)
-
-print(x)
