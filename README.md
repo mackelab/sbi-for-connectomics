ed4f6fc926799c42e0bec6ade3129d80eb4f9ca5
janfb <jan.boelts@tum.de>
2022-04-07 16:04:42 +0200

    update setup and readme

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # sbi-for-connectomics
+
 Repository for code and figures of the "SBI for connectomics" paper
 
 WIP
 
 manuscript is at https://github.com/mackelab/connectomics-inference-manuscript
+
+## Large files are provided via `git-lfs`
+
+The repository contains a number of large files that are provided via `git-lfs`. 
+See the `git-lfs` documentation for installation and general info. 
+Once `git-lfs` is installed locally, one can pull the large files contained in the repository just by cloning or pulling it. 
+
+## Examples
+
+The `notebook` folder contains three notebooks showing how to `simulate` connectomics data, how to use SBI to `infer` parameters, and how to `analyze` the resulting posteriors.
+
+## Reproducibility
+
+The `paper` folder contains notebooks for reproducing all figures presented in the main text of the paper.
+
+## Issues, questions
+
+Please reach out and create an issue if you have any questions or encounter problems with using this repository.
--- setup.py
+++ setup.py
@@ -1,16 +1,51 @@
 from setuptools import find_packages, setup
 
+# Package meta-data.
+NAME = "consbi"
+DESCRIPTION = "simulation-based inference for computational connectomics"
+URL = "https://github.com/mackelab/synaptic-rule-inference"
+AUTHOR = "Jan Bölts, Philipp Harth"
+REQUIRES_PYTHON = ">=3.6.0"
+
 REQUIRED = [
-    "sbi",
+    "matplotlib",
+    "numpy",
+    "scipy",
+    "sbi>=0.13.2",
+    "torch>=1.5.1",
+    "pyro-ppl>=1.3.1",
+    "tqdm",
+    "scikit-learn",
+    "pandas",
+    "sbibm @ git+https://github.com/mackelab/sbibm@connectomics-task#egg=sbibm",
     "svgutils==0.3.1",
     "invoke",
-    "jupyterlab",
-    "matplotlib",
 ]
 
+EXTRAS = {
+    "dev": [
+        "autoflake",
+        "black",
+        "deepdiff",
+        "flake8",
+        "isort",
+        "jupyter",
+        "pep517",
+        "pytest",
+        "pyyaml",
+    ],
+}
+
 setup(
-    name="consbi",
-    python_requires=">=3.6.0",
-    packages=find_packages(),
+    name=NAME,
+    version="0.1.0",
+    description=DESCRIPTION,
+    author=AUTHOR,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license="AGPLv3",
 )
