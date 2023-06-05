c503a16cd7652e6320a81a7d985c10d7ee3024e0
janfb <jan.boelts@tum.de>
2023-06-05 16:18:17 +0200

    add missing fonts

--- consbi/__init__.py
+++ consbi/__init__.py
@@ -1,7 +1,16 @@
 import pathlib
+import matplotlib
+
+from matplotlib.font_manager import fontManager
 
 from consbi.simulators.poisson_glm import get_prior_dist, poisson_glm
 
+# Define paths
 BASE_PATH = pathlib.Path(__file__).parent.parent
 DATA_PATH = BASE_PATH.joinpath("data")
 RESULTS_PATH = BASE_PATH.joinpath("results")
+
+# Add fonts
+fontManager.addfont(BASE_PATH / ".fonts/arial.ttf")
+matplotlib.rc("font", **{"family": "sans-serif", "sans-serif": "Arial"})
+del matplotlib
